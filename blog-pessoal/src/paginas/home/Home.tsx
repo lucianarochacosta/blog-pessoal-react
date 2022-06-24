0d2fe2605cdd51f99a24227066018f51ef0b6a08
unknown <lucianarocha_14@hotmail.com>
2022-06-24 15:49:39 -0300

    Criacao do projeto Blog Pessoal com ReactJS

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://cdn.pixabay.com/photo/2018/01/29/13/03/internet-3116062_960_720.jpg" alt="Background cinza e desenhos variados em branco." className="img"/>
+        </>
+    );
+}
+
+export default Home;
