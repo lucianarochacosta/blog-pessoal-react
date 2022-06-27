b4aa06c0cf3906358a3e79ee57fb3bc39692d6ab
unknown <lucianarocha_14@hotmail.com>
2022-06-27 16:33:12 -0300

    Features menu, home e footer com MaterialUI

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import {Grid, Paper} from '@material-ui/core';
+import { Grid, Box, Typography, Button } from '@mui/material';
 import './Home.css';
 
 function Home(){
     return(
         <>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={8}>
-                    <Paper style={{height: "100vh", background:"lightgrey"}}/>
-              </Grid>
-              <Grid item container direction="column" xs={12} sm={4} spacing={2}>
-                <Grid item>
-                    <Paper style={{height: "49vh", background:"orange"}}/>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#244673" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vinde!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Compartilhe seus pensamentos aqui!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#0D0D0D", color: "white" }}>Ver Postagens</Button>
+                    </Box>
                 </Grid>
-                <Grid item>
-                    <Paper style={{height: "49vh", background:"green"}}/>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/F2ovarm.png" alt="" width="650px" height="650px"/>
                 </Grid>
-              </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
             </Grid>
         </>
     );
