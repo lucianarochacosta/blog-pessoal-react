5fa97e6f7f124c3b8e7e4aee7ae06c2375a73f01
unknown <lucianarocha_14@hotmail.com>
2022-06-27 12:09:25 -0300

    Add dependencias do Material UI

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Grid, Paper} from '@material-ui/core';
 import './Home.css';
 
 function Home(){
     return(
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://cdn.pixabay.com/photo/2018/01/29/13/03/internet-3116062_960_720.jpg" alt="Background cinza e desenhos variados em branco." className="img"/>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={8}>
+                    <Paper style={{height: "100vh", background:"lightgrey"}}/>
+              </Grid>
+              <Grid item container direction="column" xs={12} sm={4} spacing={2}>
+                <Grid item>
+                    <Paper style={{height: "49vh", background:"orange"}}/>
+                </Grid>
+                <Grid item>
+                    <Paper style={{height: "49vh", background:"green"}}/>
+                </Grid>
+              </Grid>
+            </Grid>
         </>
     );
 }
